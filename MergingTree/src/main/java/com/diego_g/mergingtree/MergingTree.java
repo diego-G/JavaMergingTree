568a47e3cb00be6de46c0574f65cd847fa21753f
dgarcia <d.garcia@adlpartner.es>
2015-11-14 14:47:05 +0100

    - First commit

---
+++ MergingTree/src/main/java/com/diego_g/mergingtree/MergingTree.java
@@ -0,0 +1,17 @@
+package main.java.com.diego_g.mergingtree;
+
+/**
+ * @author dgarcia
+ *
+ */
+class MergingTree {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
